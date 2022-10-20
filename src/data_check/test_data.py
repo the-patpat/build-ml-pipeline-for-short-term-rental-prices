179b7012b40c662f9356b9f729efd4e0212684a0
Patrick Schmidt <patschmidt1234@gmail.com>
2022-10-20 12:46:17 +0200

    implemented data_check step

--- src/data_check/test_data.py
+++ src/data_check/test_data.py
@@ -63,3 +63,16 @@ def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame, kl_th
 ########################################################
 # Implement here test_row_count and test_price_range   #
 ########################################################
+
+def test_row_count(data):
+    """
+    Tests whether the dataset is of reasonable size
+    """
+    assert 15000 < data.shape[0] < 1000000
+
+def test_price_range(data, min_price, max_price):
+    """
+    Tests whether our cleaning step was successfull and the price is between
+    the pre-defined range
+    """
+    assert data['price'].between(min_price, max_price).all()
