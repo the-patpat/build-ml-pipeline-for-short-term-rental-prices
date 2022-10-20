8fd597c7bc69e34825bed7c33e632aaeba61f5d9
Patrick Schmidt <patschmidt1234@gmail.com>
2022-10-20 15:38:44 +0200

    implemented test regression step

--- main.py
+++ main.py
@@ -86,12 +86,20 @@ def go(config: DictConfig):
             pass
 
         if "test_regression_model" in active_steps:
+            # Download file and load in W&B
+            _ = mlflow.run(
+                f"{config['main']['components_repository']}"+
+                "/test_regression_model",
+                "main",
+                # version parameter necessary as MLFlow in the installed
+                # version looks for master instead of main
+                version=config['main']['components_version'],
+                parameters={
+                    "mlflow_model": "random_forest_export:prod",
+                    "test_dataset": "test_data.csv:latest"
+                }
+            )
 
-            ##################
-            # Implement here #
-            ##################
-
-            pass
 
 
 if __name__ == "__main__":
